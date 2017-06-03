e42ce63201f1f7a99e491961df349520dca111d5
Shaily Khare <shailykhare15@gmail.com>
2017-06-02 18:27:04 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/*Declaration of main angular module for this appllication.
+ * It is named turtleFacts and has no dependencies (hence the empty array as the second argument)
+ */
+
+(function(){
+    angular.module("turtleFacts", []);
+})();
